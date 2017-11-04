a1165ad0fcfd8c477c565ab372e914fb1aa283a5
SeanBastable <bastabls2@gmail.com>
2017-11-04 11:38:53 +0000

    Added patch to fix build issue in Kinect drivers

--- install_kinect_drivers.sh
+++ install_kinect_drivers.sh
@@ -1,6 +1,11 @@
+cd ~
 git clone git@github.com:avin2/SensorKinect.git
 sudo apt-get install libusb-1.0-0-dev freeglut3-dev
-cd SensorKinect/Platform/Linux/CreateRedist/
+cd SensorKinect
+git checkout unstable
+wget --no-check-certificate https://patch-diff.githubusercontent.com/raw/avin2/SensorKinect/pull/5.patch
+git apply 5.patch
+cd Platform/Linux/CreateRedist/
 ./RedistMaker
 cd ../Redist/Sensor-Bin-Linux-x64-v5.1.2.1/
 sudo ./install.sh
